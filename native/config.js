1dccb3c629c31d084ebe66dd4a79fa5875004a09
gooking <javazj@qq.com>
2022-02-22 15:10:56 +0800

    fixed:绑定手机号码，需要点击二次才绑定成功

--- native/config.js
+++ native/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: "2.3.0",
-  note: '微信登陆授权机制调整',
+  version: "2.3.1",
+  note: 'fixed:绑定手机号码，需要点击二次才绑定成功',
   subDomain: "aicard", // 根据教程 https://www.yuque.com/apifm/doc/qr6l4m 查看你自己的 subDomain
 }
